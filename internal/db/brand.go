fa013dbe7d2d033f2622c2ea73d4a44749282f03
Edgar Sipki <edo7796@gmail.com>
2019-07-06 15:49:01 +0300

    check

--- internal/db/brand.go
+++ internal/db/brand.go
@@ -16,9 +16,11 @@ const (
 	schemeTableBrands = `create table if not exists brands
 (
     brand_id        	 serial primary key,
-    name     		 	 text not null unique,
+    name     		 	 text not null,
 	is_delete            boolean DEFAULT false,
-	created_at 		 	 TIMESTAMPTZ NOT NULL DEFAULT NOW()
+	created_at 		 	 TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	deleted_at 		 	 TIMESTAMPTZ,
+	unique 				 (name, deleted_at)
 )`
 )
 
@@ -36,7 +38,7 @@ type (
 
 // BrandCreate - create new brand.
 func (db *db) BrandCreate(ctx Ctx, brand NewBrand) ([]Brand, error) {
-	str := "INSERT INTO " + tableBrand.name + " (" + tableBrand.columnName + ") " + "VALUES ($1) RETURNING " + tableBrand.columnID
+	str := "INSERT INTO " + tableBrand.name + " (" + tableBrand.columnName + ") " + "VALUES ($1) "
 	_, err := db.conn.ExecContext(ctx, str, brand.Name)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to set new brand")
@@ -51,12 +53,12 @@ func (db *db) getBrands(ctx Ctx) ([]Brand, error) {
 		" WHERE " + tableBrand.columnIsDelete + " <> $1"
 	err := db.conn.SelectContext(ctx, &brands, str, true)
 
-	return brands, errors.Wrapf(err, "failed to ger brands")
+	return brands, errors.Wrapf(err, "failed to get brands")
 }
 
 // BrandDelete - remove brand.
 func (db *db) BrandDelete(ctx Ctx, brandID int) (err error) {
-	str := "UPDATE " + tableBrand.name + " SET is_delete=true WHERE " + tableBrand.columnID + "= $1"
+	str := "UPDATE " + tableBrand.name + " SET is_delete=true, deleted_at = Now() WHERE " + tableBrand.columnID + "= $1"
 	_, err = db.conn.ExecContext(ctx, str, brandID)
 
 	return errors.Wrapf(err, "failed to remove brand")
